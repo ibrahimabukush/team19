7181394d12c136a2e9841418a4cd0319c0ad9759
ibrahimabukush <ibrahim.class.11@gmail.com>
2025-03-26 15:42:39 +0200

    ibrahim

--- users/apps.py
+++ users/apps.py
@@ -4,5 +4,7 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
     def ready(self):
-        import users.signals
+        # import users.signals  ← This was causing the error because signals.py no longer exists
+        pass
