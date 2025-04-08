02888e060be745f3f128eb98100c6ef08e751b60
ibrahimabukush <ibrahim.class.11@gmail.com>
2025-04-09 02:15:03 +0300

    ibrahim

--- istudent/urls.py
+++ istudent/urls.py
@@ -20,15 +20,20 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from users import views as user_views
-
+from django.contrib import admin
+from django.urls import path, include
+from users import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('login/', user_views.login_view, name='login'),
     path('profile/', user_views.profile, name='profile'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
+    path('lecturer/dashboard/', views.lecturer_dashboard, name='lecturer_dashboard'),
+    
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,20 +1,60 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from .models import Profile
+from django.contrib.auth import authenticate, login
+from .forms import UserRegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.views.decorators.csrf import requires_csrf_token
+
 
 def register(request):
+    msg = None
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
-            return redirect('login')
+            user = form.save(commit=False)
+            if user.is_student:
+                user.is_approved = True  
+            user.save()
+            msg = "Account created"
+            if user.is_student:
+                login(request, user)
+                return redirect('blog-home')
+            elif user.is_lecturer:
+                msg = "Lecturer account created and waiting approval"
+                return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def login_view(request):
+    msg = None
+    form = AuthenticationForm(request, data=request.POST or None)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            user = form.get_user()
+
+            if user.is_lecturer and not user.is_approved:
+                msg = "Lecturer account is awaiting admin approval."
+            else:
+                login(request, user)
+
+                # Redirect based on role
+                if user.is_student:
+                    return redirect('blog-home')
+                elif user.is_lecturer:
+                    return redirect('lecturer_dashboard')
+               
+
+        else:
+            msg = "Invalid username or password."
+
+    return render(request, 'users/login.html', {'form': form, 'msg': msg})
+
 
 
 @login_required
@@ -41,3 +81,16 @@ def profile(request):
     }
 
     return render(request, 'users/profile.html', context)
+
+
+
+def is_approved_lecturer(user):
+    return user.is_authenticated and user.is_lecturer and user.is_approved
+
+@login_required
+@user_passes_test(is_approved_lecturer)
+def lecturer_dashboard(request):
+    return render(request, 'users/lecturer_dashboard.html')  # or 'account/lecturer_dashboard.html'
+@requires_csrf_token
+def csrf_failure(request, reason=""):
+    return render(request, 'users/csrf_failure.html', status=403, context={'reason': reason})
